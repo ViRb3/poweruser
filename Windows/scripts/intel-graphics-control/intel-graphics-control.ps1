3b67cd7bdb02c7f9033a69d04a90ae53545ca973
Victor <virb3e@gmail.com>
2020-09-29 18:30:19 +0100

    Improve script

--- Windows/scripts/intel-graphics-control/intel-graphics-control.ps1
+++ Windows/scripts/intel-graphics-control/intel-graphics-control.ps1
@@ -1,6 +1,7 @@
-Get-PnpDevice |
+$DEVICE = Get-PnpDevice |
 Where-Object { $_.FriendlyName -like 'Intel* Graphics' } | 
-ForEach-Object {
-    pnputil.exe /remove-device $_.InstanceId
-    pnputil.exe /scan-devices
-}
+Select-Object -First 1
+
+pnputil.exe /remove-device $DEVICE.InstanceId 
+pnputil.exe /scan-devices
+Stop-Process -Name "explorer"
