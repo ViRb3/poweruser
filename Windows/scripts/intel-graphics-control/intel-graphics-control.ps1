f4f602e397468ad6a1f937b5a79feabadd715d35
Victor <virb3e@gmail.com>
2020-10-03 02:53:36 +0100

    Remove unnecessary kills

--- Windows/scripts/intel-graphics-control/intel-graphics-control.ps1
+++ Windows/scripts/intel-graphics-control/intel-graphics-control.ps1
@@ -4,16 +4,3 @@ Select-Object -First 1
 
 pnputil.exe /remove-device $DEVICE.InstanceId 
 pnputil.exe /scan-devices
-
-$PROCS = "dwm", "explorer"
-foreach ($PROC in $PROCS) {
-    Stop-Process -Name $PROC -Force
-}
-
-Start-Sleep -Seconds 1
-
-foreach ($PROC in $PROCS) {
-    if (!(Get-Process -Name $PROC)) {
-        Start-Process -FilePath $PROC
-    }
-}
